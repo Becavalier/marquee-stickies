d7ba7b43b2db7ff8c6a1d3a25e66f01ee74004d1
Becavalier <yhorg@hotmail.com>
2020-05-15 21:03:04 +0800

    Update codebase.

---
+++ src/renderer/index.js
@@ -0,0 +1,33 @@
+const { ipcRenderer } = require('electron')
+const fs = require('fs');
+
+const UPDATE_INTERVAL = 5000;
+const DEFAULT_INDEX = 0;
+
+let globalContent, counter = 1;
+
+// stylesheets;
+const styles = document.createElement('style');
+styles.innerText = `html, body {font-family: sans-serif; background-color: transparent;}#app {color: #24292e;font-weight: bold;font-size: 19px;}`;
+document.head.appendChild(styles);
+
+ipcRenderer.on('asynchronous-message', (event, configFilePath) => {
+  if (!globalContent) {
+    // loading source file.
+    globalContent = fs.readFileSync(configFilePath.filePaths[DEFAULT_INDEX], {
+      encoding: 'utf8', 
+      flag: 'r',
+    }).split('\n').map(s => s.trim());
+    if (Array.isArray(globalContent) && globalContent.length > 0) {
+      const el = document.querySelector('#app');
+      el.innerText = globalContent[DEFAULT_INDEX];
+      setInterval(() => {
+        el.innerText = globalContent[counter++];
+        if (counter === globalContent.length) {
+          counter = 0;
+        }
+      }, UPDATE_INTERVAL);
+    }
+  }
+});
+
